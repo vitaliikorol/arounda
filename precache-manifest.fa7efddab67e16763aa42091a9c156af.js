5fe89646be29df685d782b3119475be077b2853e
Vitalii Korol <vitaliikorol@MacBook-Pro.local>
2020-07-25 20:32:29 +0300

    Updates

---
+++ precache-manifest.fa7efddab67e16763aa42091a9c156af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ead9e429c8a5201d81cf27d5a1173b",
+    "url": "/arounda/index.html"
+  },
+  {
+    "revision": "c994b66b177e44435a3e",
+    "url": "/arounda/static/css/main.6b4ce1e2.chunk.css"
+  },
+  {
+    "revision": "b3853837e83254565570",
+    "url": "/arounda/static/js/2.8485cec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arounda/static/js/2.8485cec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c994b66b177e44435a3e",
+    "url": "/arounda/static/js/main.7ec047fd.chunk.js"
+  },
+  {
+    "revision": "a6039bba9fd4a4cab8ab",
+    "url": "/arounda/static/js/runtime-main.1eb9199d.js"
+  }
+]);
